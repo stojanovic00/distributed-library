54a610820e7c0eca82bccb8882325954e63e4a46
Aleksandar <al.stojanovic0@gmail.com>
2024-01-05 15:14:10 +0100

    Containerzied whole app
    NS, BG, NI application and db containers made
    Exported insomnia config

--- local_library/config/config.go
+++ local_library/config/config.go
@@ -5,13 +5,13 @@ import (
 )
 
 type Config struct {
-	Host   string `long:"host" env:"LOCAL_HOST_NS"`
-	Port   string `long:"port" env:"LOCAL_PORT_NS"`
-	DbName string `long:"db-name" env:"LOCAL_DB_NAME_NS"`
-	DbHost string `long:"db-host" env:"LOCAL_DB_HOST_NS"`
-	DbPort string `long:"db-port" env:"LOCAL_DB_PORT_NS"`
-	DbUser string `long:"db-user" env:"LOCAL_DB_USER_NS"`
-	DbPass string `long:"db-pass" env:"LOCAL_DB_PASS_NS"`
+	Host   string `long:"host" env:"LOCAL_HOST"`
+	Port   string `long:"port" env:"LOCAL_PORT"`
+	DbName string `long:"db-name" env:"LOCAL_DB_NAME"`
+	DbHost string `long:"db-host" env:"LOCAL_DB_HOST"`
+	DbPort string `long:"db-port" env:"LOCAL_DB_PORT"`
+	DbUser string `long:"db-user" env:"LOCAL_DB_USER"`
+	DbPass string `long:"db-pass" env:"LOCAL_DB_PASS"`
 
 	CentralLibHost string `long:"central-host" env:"CENTRAL_HOST"`
 	CentralLibPort string `long:"central-port" env:"CENTRAL_PORT"`
